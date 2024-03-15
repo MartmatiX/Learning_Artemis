5b0f67fcc4770210936c9de37df8a79e95759ea1
MartmatiX <martin.malir@gmail.com>
2024-03-15 22:26:00 +0100

    Testing message

--- src/main/java/com/example/learning_artemis/artemis/MessageSender.java
+++ src/main/java/com/example/learning_artemis/artemis/MessageSender.java
@@ -1,5 +1,6 @@
 package com.example.learning_artemis.artemis;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Component;
 
@@ -8,8 +9,13 @@ public class MessageSender {
 
     private final JmsTemplate jmsTemplate;
 
+    @Autowired
     public MessageSender(JmsTemplate jmsTemplate) {
         this.jmsTemplate = jmsTemplate;
+    }
+
+    public void sendMessage(String destination, String message) {
+        jmsTemplate.convertAndSend(destination, message);
     }
 
 }
